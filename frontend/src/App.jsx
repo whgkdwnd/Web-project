07d71ba28947884fe2277f4bf040c1a0815db13d
shalalla <whgkdwnd@naver.com>
2026-05-29 15:13:40 +0900

    feat: animated page transitions with AnimatePresence

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 import Home from './pages/Home'
 import Join from './pages/Join'
 import CalendarPage from './pages/CalendarPage'
+
+const pageVariants = {
+  initial: { opacity: 0, y: 10 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0 },
+}
+
+function PageWrapper({ children }) {
+  return (
+    <motion.div
+      variants={pageVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{ duration: 0.3 }}
+    >
+      {children}
+    </motion.div>
+  )
+}
 
 function RequireTeam({ children }) {
   const teamCode = JSON.parse(localStorage.getItem('team_code'))
@@ -17,14 +38,23 @@ function RequireMember({ children }) {
   return children
 }
 
+function AnimatedRoutes() {
+  const location = useLocation()
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+        <Route path="/join" element={<RequireTeam><PageWrapper><Join /></PageWrapper></RequireTeam>} />
+        <Route path="/calendar" element={<RequireMember><PageWrapper><CalendarPage /></PageWrapper></RequireMember>} />
+      </Routes>
+    </AnimatePresence>
+  )
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/join" element={<RequireTeam><Join /></RequireTeam>} />
-        <Route path="/calendar" element={<RequireMember><CalendarPage /></RequireMember>} />
-      </Routes>
+      <AnimatedRoutes />
     </BrowserRouter>
   )
 }
